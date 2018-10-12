bdfb0a2514686856668fb88c2cc080225042a28e
maolopez <maolopez@users.noreply.github.com>
2018-10-12 16:26:46 -0400

    Update echodoublebackslash.sh

--- echodoublebackslash.sh
+++ echodoublebackslash.sh
@@ -13,6 +13,6 @@ rm -f prod-pass.conf
 touch prod-pass.conf
 echo -e 'secured {\ncfs-pass = '$string2'\nPromApp-user = '$string3'\nPromApp-pass = $string4\n}' > prod-pass.conf
 cat prod-pass.conf | sed -i '/secured {/a cfs-user = "production\\\\frontend"' prod-pass.conf | /cinder/app1/scripts/jq .
-cat preprod-pass.conf | sed -i '/PromApp-user = "MyPromAppUser"/a PromApp-pass = "MyPromAppPass!"' prod-pass.conf | /cinder/app1/scripts/jq .
+cat prod-pass.conf | sed -i '/PromApp-user = "MyPromAppUser"/a PromApp-pass = "MyPromAppPass!"' prod-pass.conf | /cinder/app1/scripts/jq .
 cd /app/cfs_downloader/current
 nohup java -Dconfig.resource=/prod-pass.conf -cp /app/Jencomart/pass:./cfs-downloader-18.4.3.jar ca.Jencomart.downloader.CfsDownloader flush > foo.out 2> foo.err < /dev/null &
